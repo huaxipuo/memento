d8f05e0f7b02e98be3fb8892fcab3931169b06f8
huaxipuo <109389275+huaxipuo@users.noreply.github.com>
2022-07-18 15:25:16 +0200

    Update convert.js

--- convert.js
+++ convert.js
@@ -1,10 +1,10 @@
-const convert = { 
-  f1: function getWeekDate(d1) { 
+var convert = { 
+  f1: function (d1) { 
   pd = new Date(d1); 
   var day = pd.getDay(); 
   var weeks = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"); 
   var week = weeks[day]; 
-  return week; 
+  return week;
   },
 
-}
+};
